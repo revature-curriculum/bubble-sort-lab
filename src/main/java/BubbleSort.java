af27e540ce22dbcf2d3deafdea1c55dc6c25b56a
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-06-02 14:14:19 -0500

    add solution

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,2 +1,22 @@
 public class BubbleSort {
+
+
+    public static Integer[] sort(Integer[] integers){
+        boolean swapped = false;
+
+        do{
+            swapped = false;
+            for(int i=0; i < integers.length - 1; i++){
+                if(integers[i] > integers[i+1]){
+                    int low = integers[i+1];
+                    integers[i+1] = integers[i];
+                    integers[i] = low;
+
+                    swapped = true;
+                }
+            }
+        } while(swapped);
+
+        return integers;
+    }
 }
