7564c2e3be53efa48ce13b95ac1253754f6a089c
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-06-02 14:16:54 -0500

    add version 1 of the lab

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,2 +1,12 @@
 public class BubbleSort {
+
+    /** Lab Instructions:
+     *      Implement the sort() method such that an integer array of any
+     *      size can be assorted in ascending order.
+     */
+    public static Integer[] sort(Integer[] integers){
+        //TODO: Implement Me
+
+        return integers;
+    }
 }
