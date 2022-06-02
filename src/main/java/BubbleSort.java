f71d2bc90515af8317c1d70081fa5fe41db12656
bpinkerton <brandon.a.pinkerton@gmail.com>
2022-06-02 15:19:05 -0500

    edit lab instructions

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,8 +1,14 @@
 public class BubbleSort {
 
     /** Lab Instructions:
-     *      Implement the sort() method utilizing a Bubble Sort algorithm
-     *      such that an integer array of any size can be assorted in ascending order.
+     *      Implement the sort() method utilizing a Bubble Sort algorithm.
+     *      The method will be provided an unsorted array of integers as a parameter.
+     *      The array should be returned from the method in ascending order. Ex:
+     *              input   -> [3,2,5,1,4]
+     *              output  -> [1,2,3,4,5]
+     *
+     *      Be sure to be mindful of the array indices as ArrayIndexOutOfBoundsExceptions
+     *      are very common.
      */
     public static Integer[] sort(Integer[] integers){
         //TODO: Implement Me
